d5a06a08bba91892d513a470fb55e733ea50e553
esteban7590 <esteban7590@hotmail.com>
2021-01-05 10:02:34 -0500

    added some card styling

--- src/components/Main/EstateCard.js
+++ src/components/Main/EstateCard.js
@@ -5,22 +5,22 @@ import { loadEstates } from "../redux/actions/loadEstates";
 function EstateCard() {
   const estatesArray = useSelector((state) => state);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(loadEstates());
   }, [dispatch]);
 
   const estates = estatesArray.map((estate) => (
-    <div key={estate.id}>
-      <img src={estate.photos} alt="estate-pic" />
+    <div className="card" key={estate.id}>
+      <img className="photo" src={estate.photos} alt="estate-pic" />
       <p>Owner: {estate.owner}</p>
       <p>Kind: {estate.kind}</p>
       <p>Area: {estate.area}</p>
       <p>Description: {estate.description}</p>
-      <br />
     </div>
   ));
 
-  return <div>{estates}</div>;
+  return <div className="cards-container">{estates}</div>;
 }
 
 export default EstateCard;
